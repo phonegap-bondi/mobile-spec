32909aa261cb62ac6d89a224665ddd1ac5a02418
sph <stefan.pham@fokus.fraunhofer.de>
2010-04-13 16:24:08 +0200

    *new bondi.tests, devicestatus.tests

---
+++ tests/bondi/bondi.tests.js
@@ -0,0 +1,66 @@
+Tests.prototype.BondiTests = function() {	
+	module('Bondi (bondi)');
+	test("should exist", function() {
+  		expect(1);
+  		ok(bondi != null, "bondi should not be null.");
+	});
+
+	test("should contain a requestFeature function", function() {
+		expect(5);
+		ok(typeof bondi.requestFeature != 'undefined' && bondi.requestFeature != null, "bondi.requestFeature should not be null.");
+		ok(typeof bondi.requestFeature == 'function', "bondi.requestFeature should be a function.");
+		var win=function(){ok(true,"bondi.requestFeature must be available");};
+		var fail=function(){ok(true,"bondi.requestFeature must be available");};
+		var pendingOperation=bondi.requestFeature(win,fail,"http://bondi.omtp.org/api/1.1/messaging");
+
+		win=function(p){ok(false,"bondi.requestFeature unknown feature must raise NOT_FOUND_ERROR");};
+		fail=function(p){ok(p.code==10002,"bondi.requestFeature unknown feature must raise NOT_FOUND_ERROR");};
+		pendingOperation=bondi.requestFeature(win,fail,"http://bondi.omtp.org/api/messaging");
+
+		fail=function(p){ok(p.code==10001,"bondi.requestFeature invalid success callback must raise INVALID_ARGUMENT_ERROR");};
+		pendingOperation=bondi.requestFeature(null,fail,"http://bondi.omtp.org/api/messaging");		
+	});
+
+	
+	test("should contain a getFeatures function", function() {
+		expect(7);
+		ok(typeof bondi.getFeatures != 'undefined' && bondi.getFeatures != null, "bondi.getFeatures should not be null.");
+		ok(typeof bondi.getFeatures == 'function', "bondi.getFeatures should be a function.");
+
+		var featurelist=bondi.getFeatures();
+		ok(typeof featurelist != 'undefined' && featurelist != null && featurelist.length>=0, "bondi.getFeatures should return a featurelist.");
+		
+		var featureArray=new Array();
+		featureArray[0]="http://bondi.omtp.org/api/1.1/filesystem";
+		featureArray[1]="http://bondi.omtp.org/api/1.1/devicestatus";
+		featureArray[2]="http://bondi.omtp.org/api/1.1/geolocation";
+		featureArray[3]="http://bondi.omtp.org/api/1.1/camera";
+		
+		for (i = 0; i < featureArray.length; i++)
+		{
+			var found=false;
+			for (j=0; j<featurelist.length; j++)
+			{
+				if (featureArray[i] == featurelist[j])
+				{
+					found=true;
+				}
+			}
+			ok(found, "Feature "+featureArray[i]+" should exist.")				
+		}
+	});
+
+
+	test("should contain a cancel function", function() {
+		expect(3);
+		var pendingOperation = new PendingOperation();
+		ok(typeof pendingOperation.cancel != 'undefined' && pendingOperation.cancel != null, "PendingOperation.cancel should not be null.");
+		ok(typeof pendingOperation.cancel == 'function', "PendingOperation.cancel should be a function.");
+		var win=function(){};
+		var fail=function(){};
+		pendingOperation=bondi.requestFeature(win,fail,"http://bondi.omtp.org/api/messaging");
+		if (typeof pendingOperation == 'object' && pendingOperation.cancel()==false)
+			ok(true,"PendingOperation.cancel must return false on unavailable features");
+	});
+	
+};
---
+++ tests/bondi/devicestatus.tests.js
@@ -0,0 +1,222 @@
+Tests.prototype.DeviceTests = function() {
+	module('DeviceStatusManager (bondi.devicestatus)');
+	test("should exist", function() {
+  		expect(1);
+  		ok(bondi.devicestatus != null, "bondi.devicestatus should not be null.");
+	});
+	
+    test("should contain a listVocabularies function", function() {
+        expect(3);
+        ok(typeof bondi.devicestatus.listVocabularies != 'undefined' && bondi.devicestatus.listVocabularies != null, "bondi.devicestatus.listVocabularies should not be null.");
+        ok(typeof bondi.devicestatus.listVocabularies == 'function', "bondi.devicestatus.listVocabularies should be a function.");
+		var value = bondi.devicestatus.listVocabularies();
+		ok(typeof value != 'undefined' && value != null && value.length>0, "list of vocabularies should not be null");
+	});
+    
+    test("should contain a setDefaultVocabulary function", function() {
+        expect(7);
+        ok(typeof bondi.devicestatus.setDefaultVocabulary != 'undefined' && bondi.devicestatus.setDefaultVocabulary != null, "bondi.devicestatus.setDefaultVocabulary should not be null.");
+        ok(typeof bondi.devicestatus.setDefaultVocabulary == 'function', "bondi.devicestatus.setDefaultVocabulary should be a function.");
+		var value = bondi.devicestatus.listVocabularies();
+		try
+		{
+		  bondi.devicestatus.setDefaultVocabulary(value[0]);
+		  ok(true,"A default vocabulary must always be available");
+		}
+		catch(myError)
+		{
+		  fail(true,"A default vocabulary must always be available");
+		}	
+	
+		try
+		{
+		  bondi.devicestatus.setDefaultVocabulary("bonditest");
+		  fail(true,"One may not set a non-existent vocabulary (INVALID_ARGUMENT_ERROR)");
+		}
+		catch(myError)
+		{
+		  ok(myError.code==10002,"One may not set a non-existent vocabulary (INVALID_ARGUMENT_ERROR)");
+		}	
+	
+		try
+		{
+		  bondi.devicestatus.setDefaultVocabulary(null);
+		  fail(true,"One may not set a null vocabulary (INVALID_ARGUMENT_ERROR)");
+		}
+		catch(myError)
+		{
+		  ok(myError.code==10002,"One may not set a null vocabulary (INVALID_ARGUMENT_ERROR)");
+		}	
+	
+		try
+		{
+		  bondi.devicestatus.setDefaultVocabulary('');
+		  fail(true,'One may not set an empty string as vocabulary (INVALID_ARGUMENT_ERROR)');
+		}
+		catch(myError)
+		{
+		  ok(myError.code==10002,'One may not set an empty string as vocabulary (INVALID_ARGUMENT_ERROR)');
+		}	
+	
+		try
+		{
+		  var undefinedTestVariable;
+		  bondi.devicestatus.setDefaultVocabulary(undefinedTestVariable);
+		  fail(true,'One may not set an undefined variable as vocabulary (INVALID_ARGUMENT_ERROR)');
+		}
+		catch(myError)
+		{
+		  ok(myError.code==10002,'One may not set an undefined variable as vocabulary (INVALID_ARGUMENT_ERROR)');
+		}
+	});
+	
+    test("should contain a listAspects function", function() {
+        expect(3);
+        ok(typeof bondi.devicestatus.listAspects != 'undefined' && bondi.devicestatus.listAspects != null, "bondi.devicestatus.listAspects should not be null.");
+        ok(typeof bondi.devicestatus.listAspects == 'function', "bondi.devicestatus.listAspects should be a function.");
+		var value = bondi.devicestatus.listAspects();
+		ok(typeof value != 'undefined' && value.length>0, "bondi.devicestatus.listAspects must always deliver at least on value");
+		});
+    
+    test("should contain a getComponents function", function() {
+        expect(3);
+        ok(typeof bondi.devicestatus.getComponents != 'undefined' && bondi.devicestatus.getComponents != null, "bondi.devicestatus.getComponents should not be null.");
+        ok(typeof bondi.devicestatus.getComponents == 'function', "bondi.devicestatus.getComponents should be a function.");
+        var value = bondi.devicestatus.listAspects();
+		var components = bondi.devicestatus.getComponents({aspect:value[0]});
+		ok(typeof components != 'undefined' && components.length>0, "an aspect must always have at least one component");
+	});
+    
+    
+    test("should contain a listProperties function", function() {
+        expect(3);
+        ok(typeof bondi.devicestatus.listProperties != 'undefined' && bondi.devicestatus.listProperties != null, "bondi.devicestatus.listProperties should not be null.");
+        ok(typeof bondi.devicestatus.listProperties == 'function', "bondi.devicestatus.listProperties should be a function.");
+		var value = bondi.devicestatus.listAspects();
+		var properties = bondi.devicestatus.listProperties({aspect:value[0]});
+		ok(typeof properties != 'undefined' && properties.length>0, "an aspect must always have at least one property");
+	});
+    
+    test("should contain a getPropertyValue function", function() {
+        expect(3);
+        ok(typeof bondi.devicestatus.getPropertyValue != 'undefined' && bondi.devicestatus.getPropertyValue != null, "bondi.devicestatus.getComponents should not be null.");
+        ok(typeof bondi.devicestatus.getPropertyValue == 'function', "bondi.devicestatus.getPropertyValue should be a function.");
+		var value = bondi.devicestatus.listAspects();
+		var properties = bondi.devicestatus.listProperties({aspect:value[0]});
+		var level = bondi.devicestatus.getPropertyValue({property:properties[0]});
+		ok(typeof level != 'undefined' && level != null, "An aspect has at least one valid property set"); 
+	});
+
+    test("should contain a clearPropertyChange function", function() {
+        expect(2);
+        ok(typeof bondi.devicestatus.clearPropertyChange != 'undefined' && bondi.devicestatus.clearPropertyChange != null, "bondi.devicestatus.clearPropertyChange should not be null.");
+        ok(typeof bondi.devicestatus.clearPropertyChange == 'function', "bondi.devicestatus.clearPropertyChange should be a function.");
+	});
+    
+	
+    test("should contain a watchPropertyChange function", function() {
+        expect(5);
+        ok(typeof bondi.devicestatus.watchPropertyChange != 'undefined' && bondi.devicestatus.watchPropertyChange != null, "bondi.devicestatus.watchPropertyChange should not be null.");
+        ok(typeof bondi.devicestatus.watchPropertyChange == 'function', "bondi.devicestatus.watchPropertyChange should be a function.");
+		var numwatchcallleft = 3;
+		stop(tests.TEST_TIMEOUT); 
+		var batteryChangeHandler = bondi.devicestatus.watchPropertyChange({aspect:"Battery", property:"batteryLevel"},															  
+											  function onPropertyChange(ref, value) {
+											  numwatchcallleft--;
+											  if (numwatchcallleft==0){
+											  var exception = false;
+											  try {
+											  bondi.devicestatus.clearPropertyChange(batteryChangeHandler);
+											  } catch (e) { exception = true; fail(true,"clearing property change watch failed");}
+											  if (!exception)
+											  ok(numwatchcallleft==0,"clearing property change watch succeeded");
+											  start();
+											  }
+												}
+												 , {minTimeout:1000,callCallbackOnRegister:true});		 
+		var exception = false;
+		try
+		{
+			var batteryChangeHandler = bondi.devicestatus.watchPropertyChange({aspect:"battery", property:"batterylevel"}, 
+	                null, {
+	                        minTimeout:1000,
+	                        callCallbackOnRegister:true
+	                }
+	        );
+		}
+		catch(myException)
+		{
+		exception = true;
+		ok(myException.code==10001,"The watch property success callback may not be null (INVALID_ARGUMENT_ERROR)");
+		}			
+	
+		if (!exception)
+			fail(true,"The watch property success callback may not be null (INVALID_ARGUMENT_ERROR)");
+
+		 
+		 exception = false;
+		 try
+		 {
+		 var batteryChangeHandler = bondi.devicestatus.watchPropertyChange({aspect:"battery", property:"batterylevels"}, 
+					function onPropertyChange(ref, value) {}, {
+	                        minTimeout:1000,
+	                        callCallbackOnRegister:true
+	                }
+	        );
+		 }
+		 catch (myException)
+		 {
+		 exception = true;
+		 ok(myException.code==10001,"Invalid property should throw INVALID_ARGUMENT_ERROR");
+		 }	
+
+		 if (!exception)
+		 fail(true,"Invalid property should throw INVALID_ARGUMENT_ERROR");
+		
+	});
+		
+	test("should contain OS attributes", function() {
+		expect(4);
+        var value = bondi.devicestatus.getPropertyValue({property:"name", aspect:"OperatingSystem", component:"_default"});
+		ok(typeof value != 'undefined' && value != null, "name should not be null.")
+        value = bondi.devicestatus.getPropertyValue({property:"version", aspect:"OperatingSystem", component:"_default"});
+		ok(typeof value != 'undefined' && value != null, "version should not be null.") 
+        value = bondi.devicestatus.getPropertyValue({property:"language", aspect:"OperatingSystem", component:"_default"});
+		ok(typeof value != 'undefined' && value != null, "language should not be null.") 
+        value = bondi.devicestatus.getPropertyValue({property:"vendor", aspect:"OperatingSystem", component:"_default"});
+		ok(typeof value != 'undefined' && value != null, "vendor should not be null.") 
+	});
+	
+    test("should contain Battery attributes", function() {
+		expect(3);
+        var value = bondi.devicestatus.getPropertyValue({property:"batteryLevel", aspect:"Battery", component:"_default"});
+		ok(typeof value != 'undefined' && value != null, "batteryLevel should not be null.")
+        value = bondi.devicestatus.getPropertyValue({property:"batteryBeingCharged"});
+		ok(typeof value != 'undefined' && value != null, "batteryBeingCharged should not be null.") 
+        value = bondi.devicestatus.getPropertyValue({property:"batteryTechnology"});
+		ok(typeof value != 'undefined' && value != null, "batteryTechnology should not be null.") 
+	});
+    var watchID = 0;
+    
+    test("watchPropertyChange success callback should be called", function() {
+		expect(2);
+		stop(tests.TEST_TIMEOUT);
+		var win = function(ref,value) {
+			ok(ref != null, "PropertyRef object returned in watchPropertyChange success callback should not be null.");
+			ok(value != null, "new Value object returned in watchPropertyChange success callback should not be null.");			
+			start();
+		};
+		var fail = function() { start(); };
+		watchID = bondi.devicestatus.watchPropertyChange({aspect:"Battery", property:"batteryLevel"}, win, {minTimeout:5000});
+    });
+    
+	test("clearPropertyChange should stop watchPropertyChange success callbacks", function () {
+		 expect(1);
+		 try {
+			bondi.devicestatus.clearPropertyChange(watchID);
+		 } catch(error) {
+			ok( true, "bondi.devicestatus.clearPropertyChange should be able to stop property changes.");
+		 }
+		 ok( true, "bondi.devicestatus.clearPropertyChange should be able to stop property changes.");
+	});
+};
